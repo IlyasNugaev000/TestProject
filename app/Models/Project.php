f7104bb726c21a3f9449487913a2a62244bd0dfa
IlyasNugaev000 <ilyas-nugaev@mail.ru>
2024-04-04 01:19:59 +0300

    feat(): add laravel ide helper

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,6 +6,30 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
+/**
+ * App\Models\Project
+ *
+ * @property string $id
+ * @property string $name
+ * @property string $description
+ * @property string $status
+ * @property int $creator_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\User> $users
+ * @property-read int|null $users_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Project newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Project newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Project query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereCreatorId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Project whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Project extends Model
 {
     use HasFactory;
@@ -20,8 +44,8 @@ class Project extends Model
     protected static function boot()
     {
         parent::boot();
-        static::creating(function ($post) {
-            $post->{$post->getKeyName()} = (string) Str::uuid();
+        static::creating(function ($project) {
+            $project->{$project->getKeyName()} = (string) Str::uuid();
         });
     }
 
--- app/Models/UserProjectRole.php
+++ app/Models/UserProjectRole.php
@@ -5,6 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\UserProjectRole
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $project_id
+ * @property int $role_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|UserProjectRole newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserProjectRole newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserProjectRole query()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserProjectRole whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserProjectRole whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserProjectRole whereProjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserProjectRole whereRoleId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserProjectRole whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserProjectRole whereUserId($value)
+ * @mixin \Eloquent
+ */
 class UserProjectRole extends Model
 {
     use HasFactory;
